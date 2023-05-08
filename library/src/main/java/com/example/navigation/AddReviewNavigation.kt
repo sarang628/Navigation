f0c79f0e37ca32e67af41d476d123f183ba33d78
SRYANG <sarang628@naver.com>
2023-05-08 21:22:57 +0800

    update

--- library/src/main/java/com/example/navigation/AddReviewNavigation.kt
+++ library/src/main/java/com/example/navigation/AddReviewNavigation.kt
@@ -5,3 +5,11 @@ import androidx.fragment.app.Fragment
 interface AddReviewNavigation {
     fun navigate(fragment: Fragment)
 }
+
+interface FeedNavigations {
+    fun goAddReview()
+    fun goProfile()
+    fun goComment()
+    fun goRestaurant()
+    fun goFullImage()
+}
